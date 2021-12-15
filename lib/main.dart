b1436e2e9373a707b1f2cf15c580f12f949c176c
Leone <leone.souza@edu.unirio.br>
2021-12-15 13:23:13 -0300

    <feat>implement cep search input & progress HUD

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 import 'package:atlantic_flutter_challenge/requests.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +13,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -36,15 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,7 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //Api values
   String _addressCep = "";
   String _addressState = "";
   String _addressCity = "";
@@ -60,93 +43,119 @@ class _MyHomePageState extends State<MyHomePage> {
   String _addressStreet = "";
   String _addressService = "";
 
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  void _getAddress(){
-    setState(() {
-
-    });
-  }
+  //Form control
+  String _inputCep = "";
+  bool _isValid = false;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Cep: $_addressCep',
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  validator: (String? value) => validateCep(value!),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter((9)),
+                  ],
+                  onChanged: (value) {
+                    _inputCep = value;
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a search term',
+                      suffixIcon: IconButton(
+                          onPressed: () async {
+                            setState(() => showSpinner = true);
+                            await findCep();
+                            setState(() {
+                              showSpinner = false;
+                            });
+                          },
+                          icon: Icon(Icons.search))),
+                ),
+
+                //,
+                Text(
+                  'Cep: $_addressCep',
+                ),
+                Text(
+                  'Estado: $_addressState',
+                ),
+                Text(
+                  'Cidade: $_addressCity',
+                ),
+                Text(
+                  'Bairro: $_addressNeighborhood',
+                ),
+                Text(
+                  'Rua: $_addressStreet',
+                ),
+                Text(
+                  'serviço: $_addressService',
+                ),
+              ],
             ),
-            Text(
-              'Estado: $_addressState',
-            ),
-            Text(
-              'Cidade: $_addressCity',
-            ),
-            Text(
-              'Bairro: $_addressNeighborhood',
-            ),
-            Text(
-              'Rua: $_addressStreet',
-            ),
-            Text(
-              'serviço: $_addressService',
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          Response response = await getCep("20511000");
-          setState(() {
-            Map<String, Object?> addressMap = jsonDecode(response.body);
-            _addressCep = addressMap["cep"] as String;
-            _addressState = addressMap["state"] as String;
-            _addressCity = addressMap["city"] as String;
-            _addressNeighborhood = addressMap["neighborhood"] as String;
-            _addressStreet = addressMap["street"] as String;
-            _addressService = addressMap["service"] as String;
-          });
+          setState(() => showSpinner = true);
+          if (_isValid) {
+            Response response = await getCep(_inputCep);
+            setState(() {
+              Map<String, Object?> addressMap = jsonDecode(response.body);
+              _addressCep = addressMap["cep"] as String;
+              _addressState = addressMap["state"] as String;
+              _addressCity = addressMap["city"] as String;
+              _addressNeighborhood = addressMap["neighborhood"] as String;
+              _addressStreet = addressMap["street"] as String;
+              _addressService = addressMap["service"] as String;
+            });
+          }
+          setState(() => showSpinner = false);
         },
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+
+  validateCep(String value) {
+    RegExp exp = RegExp(r"^\d{5}-\d{3}");
+    if (exp.hasMatch(value)) {
+      _isValid = true;
+      return null;
+    }
+    _isValid = false;
+    return ("Insira o CEP no formato 00000-000.");
+  }
+
+  findCep() async {
+    if (_isValid) {
+      Response response = await getCep(_inputCep);
+      if (response.statusCode == 200) {
+        setState(() {
+          Map<String, Object?> addressMap = jsonDecode(response.body);
+          _addressCep = addressMap["cep"] as String;
+          _addressState = addressMap["state"] as String;
+          _addressCity = addressMap["city"] as String;
+          _addressNeighborhood = addressMap["neighborhood"] as String;
+          _addressStreet = addressMap["street"] as String;
+          _addressService = addressMap["service"] as String;
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("CEP $_inputCep não encontrado")));
+      } 
+    }
   }
 }
